4ac47cd197de483f901c85313d0545543ae9398b
CdottOTS <cbozeman4747@gmail.com>
2024-05-21 19:18:37 -0400

    Update and rename KeyGeneration to KeyGeneration.js

---
+++ KeyGeneration.js
@@ -0,0 +1,7 @@
+var Code = Math.floor(Math.random() * 90000000000000000) + 10000000000000000;
+// Test the function
+var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var random_letter = letters.charAt(Math.floor(Math.random() * letters.length));
+var Starter = "OTS/"
+var Key = ""+ Starter + "" + Code + "" + random_letter + " "
+console.log(Key)
